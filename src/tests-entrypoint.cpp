7ab7959954723e090ee635506fc44d4b0e599c60
Tomas Prerovsky <cepsdev@hotmail.com>
2023-05-01 22:40:16 +0200

    First test is successful

--- src/tests-entrypoint.cpp
+++ src/tests-entrypoint.cpp
@@ -26,7 +26,49 @@ namespace cepsplugin{
     static const std::string version_info = "INSERT_NAME_HERE v0.1";
     static constexpr bool print_debug_info{true};
     ceps::ast::node_t plugin_entrypoint(ceps::ast::node_callparameters_t params);
+    ceps::ast::node_t obj_type_as_str(ceps::ast::node_callparameters_t params);
 }
+
+#include <tuple>
+
+namespace rt{
+    using precision_t = double;
+    using tuple_base_t = std::tuple<precision_t,precision_t,precision_t,precision_t>;
+    struct tuple_t: public tuple_base_t{
+        using val_t = precision_t;
+        tuple_t(precision_t a,precision_t b,precision_t c,precision_t d):tuple_base_t{a,b,c,d}{}
+    };
+
+    tuple_t  mk_tuple(ceps::ast::Struct);
+    ceps::ast::node_struct_t mk_tuple(tuple_t);  
+}
+
+rt::tuple_t rt::mk_tuple(ceps::ast::Struct s){
+    auto& v{children(s)}; using namespace ceps::ast;
+    rt::tuple_t tuple{ 
+        v.size() > 0 && is<Ast_node_kind::float_literal>(v[0]) ? value(as_double_ref(v[0])) :rt::tuple_t::val_t{},
+        v.size() > 0 && is<Ast_node_kind::float_literal>(v[1]) ? value(as_double_ref(v[1])) :rt::tuple_t::val_t{},
+        v.size() > 0 && is<Ast_node_kind::float_literal>(v[2]) ? value(as_double_ref(v[2])) :rt::tuple_t::val_t{},
+        v.size() > 0 && is<Ast_node_kind::float_literal>(v[3]) ? value(as_double_ref(v[3])) :rt::tuple_t::val_t{}
+    };
+    return tuple; 
+}
+
+ceps::ast::node_struct_t rt::mk_tuple(rt::tuple_t tuple){
+    using namespace ceps::ast;using namespace ceps::interpreter;
+    auto result = mk_struct("tuple");
+    auto x = mk_struct("x");auto y = mk_struct("y");auto z = mk_struct("z");auto w = mk_struct("w");
+    children(*result).push_back(x);
+    children(*result).push_back(y);
+    children(*result).push_back(z);
+    children(*result).push_back(w);
+    
+    children(*x).push_back(mk_double_node(get<0>(tuple),all_zero_unit()));
+    children(*y).push_back(mk_double_node(get<1>(tuple),all_zero_unit()));
+    children(*z).push_back(mk_double_node(get<2>(tuple),all_zero_unit()));
+    children(*w).push_back(mk_double_node(get<3>(tuple),all_zero_unit()));
+    return result;
+}  
 
 ceps::ast::node_t cepsplugin::plugin_entrypoint(ceps::ast::node_callparameters_t params){
     using namespace std;
@@ -36,19 +78,35 @@ ceps::ast::node_t cepsplugin::plugin_entrypoint(ceps::ast::node_callparameters_t
     auto data = get_first_child(params);    
     if (!is<Ast_node_kind::structdef>(data)) return nullptr;
     auto& ceps_struct = *as_struct_ptr(data);
-    cout << "cepsplugin::plugin_entrypoint:\n";
+    /*cout << "cepsplugin::plugin_entrypoint:\n";
     for(auto e : children(ceps_struct)){
         cout <<"\t"<< * e << "\n";
     }
-    cout <<"\n\n";
+    cout <<"\n\n";*/
+    if (name(ceps_struct) == "tuple"){
+        auto tuple{rt::mk_tuple(ceps_struct)};
+        return rt::mk_tuple(tuple);
+    }
     auto result = mk_struct("result");
     children(*result).push_back(mk_int_node(42));
     return result;
 }
+ceps::ast::node_t cepsplugin::obj_type_as_str(ceps::ast::node_callparameters_t params){
+    using namespace std;
+    using namespace ceps::ast;
+    using namespace ceps::interpreter;
+
+    auto data = get_first_child(params);    
+    if (!is<Ast_node_kind::structdef>(data)) return nullptr;
+    auto& ceps_struct = *as_struct_ptr(data);
+    return mk_string(name(ceps_struct));
+}
+
 
 extern "C" void init_plugin(IUserdefined_function_registry* smc)
 {
   cepsplugin::plugin_master = smc->get_plugin_interface();
-  cepsplugin::plugin_master->reg_ceps_phase0plugin("INSERT_NAME_FOR_FUNCTION_HERE", cepsplugin::plugin_entrypoint);
+  cepsplugin::plugin_master->reg_ceps_phase0plugin("rt_obj", cepsplugin::plugin_entrypoint);
+  cepsplugin::plugin_master->reg_ceps_phase0plugin("rt_obj_type_as_str", cepsplugin::obj_type_as_str);
 }					
 				
