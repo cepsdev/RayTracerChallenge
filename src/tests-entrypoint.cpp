a696be15d7c8d618de64def9ede685182c6af718
Tomas Prerovsky <cepsdev@hotmail.com>
2023-05-02 13:38:41 +0200

    Add tuples

--- src/tests-entrypoint.cpp
+++ src/tests-entrypoint.cpp
@@ -27,6 +27,7 @@ namespace cepsplugin{
     static constexpr bool print_debug_info{true};
     ceps::ast::node_t plugin_entrypoint(ceps::ast::node_callparameters_t params);
     ceps::ast::node_t obj_type_as_str(ceps::ast::node_callparameters_t params);
+    ceps::ast::node_t op(ceps::ast::node_callparameters_t params);
 }
 
 #include <tuple>
@@ -36,8 +37,11 @@ namespace rt{
     using tuple_base_t = std::tuple<precision_t,precision_t,precision_t,precision_t>;
     struct tuple_t: public tuple_base_t{
         using val_t = precision_t;
+        tuple_t() = default;
         tuple_t(precision_t a,precision_t b,precision_t c,precision_t d):tuple_base_t{a,b,c,d}{}
     };
+
+    inline tuple_t operator + (tuple_t l, tuple_t r) { return {get<0>(l)+get<0>(r),get<1>(l)+get<1>(r),get<2>(l)+get<2>(r),get<3>(l)+get<3>(r)  };}
 
     tuple_t  mk_tuple(ceps::ast::Struct);
     ceps::ast::node_struct_t mk_tuple(tuple_t);  
@@ -84,6 +88,23 @@ ceps::ast::node_struct_t rt::mk_tuple(rt::tuple_t tuple){
     return result;
 }  
 
+rt::tuple_t tuple_from_ceps(ceps::ast::Struct& ceps_struct){
+    if (name(ceps_struct) == "tuple"){
+        return rt::mk_tuple(ceps_struct);
+    }
+    if (name(ceps_struct) == "point"){
+        auto tuple{rt::mk_tuple(ceps_struct)};
+        get<3>(tuple) = 1.0;
+        return tuple;
+    }
+    if (name(ceps_struct) == "vector"){
+        auto tuple{rt::mk_tuple(ceps_struct)};
+        get<3>(tuple) = 0.0;
+        return tuple;
+    }
+    return {};
+}
+
 ceps::ast::node_t cepsplugin::plugin_entrypoint(ceps::ast::node_callparameters_t params){
     using namespace std;
     using namespace ceps::ast;
@@ -97,25 +118,37 @@ ceps::ast::node_t cepsplugin::plugin_entrypoint(ceps::ast::node_callparameters_t
         cout <<"\t"<< * e << "\n";
     }
     cout <<"\n\n";*/
-    if (name(ceps_struct) == "tuple"){
-        auto tuple{rt::mk_tuple(ceps_struct)};
-        return rt::mk_tuple(tuple);
-    }
-    if (name(ceps_struct) == "point"){
-        auto tuple{rt::mk_tuple(ceps_struct)};
-        get<3>(tuple) = 1.0;
-        return rt::mk_tuple(tuple);
-    }
-    if (name(ceps_struct) == "vector"){
-        auto tuple{rt::mk_tuple(ceps_struct)};
-        get<3>(tuple) = 0.0;
-        return rt::mk_tuple(tuple);
+
+    if (name(ceps_struct) == "tuple" || name(ceps_struct) == "point" || name(ceps_struct) == "vector"){
+        return rt::mk_tuple(tuple_from_ceps(ceps_struct));
     }
 
-    auto result = mk_struct("result");
-    children(*result).push_back(mk_int_node(42));
+    auto result = mk_struct("error");
+    children(*result).push_back(mk_int_node(0));
     return result;
 }
+
+ceps::ast::node_t cepsplugin::op(ceps::ast::node_callparameters_t params){
+    using namespace std;
+    using namespace ceps::ast;
+    using namespace ceps::interpreter;
+
+    auto data = get_first_child(params);    
+    if (!is<Ast_node_kind::structdef>(data)) return nullptr;
+    auto& ceps_struct = *as_struct_ptr(data);
+    if (name(ceps_struct) == "plus"){
+        if (children(ceps_struct).size() > 1){
+            auto l = tuple_from_ceps(*as_struct_ptr(children(ceps_struct)[0]));
+            auto r = tuple_from_ceps(*as_struct_ptr(children(ceps_struct)[1]));
+            auto result = l + r;
+            return rt::mk_tuple(result);
+        }
+    }
+    auto result = mk_struct("error");
+    children(*result).push_back(mk_int_node(0));
+    return result;
+}
+
 ceps::ast::node_t cepsplugin::obj_type_as_str(ceps::ast::node_callparameters_t params){
     using namespace std;
     using namespace ceps::ast;
@@ -138,5 +171,7 @@ extern "C" void init_plugin(IUserdefined_function_registry* smc)
   cepsplugin::plugin_master = smc->get_plugin_interface();
   cepsplugin::plugin_master->reg_ceps_phase0plugin("rt_obj", cepsplugin::plugin_entrypoint);
   cepsplugin::plugin_master->reg_ceps_phase0plugin("rt_obj_type_as_str", cepsplugin::obj_type_as_str);
+  cepsplugin::plugin_master->reg_ceps_phase0plugin("rt_op", cepsplugin::op);
 }					
 				
+                
